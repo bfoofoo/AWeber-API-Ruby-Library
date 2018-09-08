e850c279f1b0c9c8a8b76b7b64a49ba0a0974c83
Egor Romanov <unsaidxpl@gmail.com>
2018-09-08 18:12:52 +0300

    add methods to campaign

--- lib/aweber/resources/activity.rb
+++ lib/aweber/resources/activity.rb
@@ -6,6 +6,30 @@ module AWeber
       api_attr :type
       api_attr :event_time
       api_attr :subscriber_link
+
+      def event
+        return nil unless %w(open click).include?(type)
+        response = client.get(self_link)
+
+        type_class = AWeber::Resources.const_get(type.classify)
+        type_class.new(client, response)
+      end
+
+      def message
+        match = self_link.match(/^(.*?)messages\/\d+/)
+        return nil unless match
+        url = match[0]
+        response = client.get(url)
+        Message.new(client, response)
+      end
+
+      def campaign
+        match = self_link.match(/^(.*?)campaigns\/[bf]\d+/)
+        return nil unless match
+        url = match[0]
+        response = client.get(url)
+        Campaign.new(client, response)
+      end
     end
   end
 end
